9061b15f25691963b9a45d347e3cd6fe5a683f15
Simon W. Jackson <189379+simonwjackson@users.noreply.github.com>
2024-11-27 12:36:15 -0600

    feat: add proton index key

--- agenix/secrets.nix
+++ agenix/secrets.nix
@@ -74,6 +74,7 @@ in {
 
   "proton-0-usenet.age".publicKeys = users ++ systems;
   "proton-0-soulseek.age".publicKeys = users ++ systems;
+  "proton-0-index.age".publicKeys = users ++ systems;
   "proton-0.age".publicKeys = users ++ systems;
   "proton-1.age".publicKeys = users ++ systems;
   "proton-2.age".publicKeys = users ++ systems;
